5840fb02ddf796eeae9f83805b38a0a364f2ffa8
Bianca <bianca.mularoni@me.com>
2024-05-14 11:31:26 +0100

    Minor changes from solution (img + add h1 with name on App component)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from "./components/SearchBar/SearchBar";
 
@@ -20,6 +21,7 @@ const businesses = [sampleBusiness, sampleBusiness, sampleBusiness, sampleBusine
 function App() {
   return (
     <div className="App">
+      <h1>ravenous</h1>
       <SearchBar />
       <BusinessList businesses={businesses} />
     </div>

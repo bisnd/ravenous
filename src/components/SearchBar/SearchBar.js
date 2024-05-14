71413b04032faecdc88459c51523435fe8bc3944
Bianca <bianca.mularoni@me.com>
2024-05-14 14:48:14 +0100

    Edit sortBy to style with JavaScript + Set State

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,40 +1,83 @@
-import React from 'react';
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 import styles from './SearchBar.module.css';
 
-const sortBy = [
-    {"Best Match": "sort_by=best_match&limit=20"},
-    {"Highest Rated": "sort_by=rating&limit=20"},
-    {"Most Reviewed": "sort_by=review_count&limit=20"}
-];
+const sortBy = {
+    "Best Match": "sort_by=best_match&limit=20",
+    "Highest Rated": "sort_by=rating&limit=20",
+    "Most Reviewed": "sort_by=review_count&limit=20"
+};
 
 function SearchBar () {
+    const [sortingOption, setSortingOption] = useState("sort_by=best_match&limit=20");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchLocation, setSearchLocation] = useState("");
+
+    function sortOptionStyle (sortOptionValue) {
+        if (sortingOption === sortOptionValue) { // Checking if the current state (sortingOption) is equal to the specific button value (sortOptionValue), which is being passed in the className
+            return styles.active;
+        } else {
+            return styles.sortButton;
+        };
+    };
+
+    function handleSortingOption (sortOptionValue) {
+        setSortingOption(sortOptionValue);
+    }
+
+    function handleSearchTerm ({target}) {
+        setSearchTerm(target.value);
+    }
+
+    function handleSearchLocation ({target}) {
+        setSearchLocation(target.value);
+    }
+
+    function handleSubmit (event) {
+        event.preventDefault(); // Prevents the form from performing the default action
+        console.log(`Searching Yelp with ${searchTerm}, ${searchLocation}, ${sortingOption}`)
+    }
+
     return (
         <div className={styles.banner}>
             <div>
-                <ToggleButtonGroup type="radio" name="sortOptions" defaultValue={0} className={styles.sortGroup}>
-                    {sortBy.map((sortOption, index) => (
-                        <ToggleButton
-                            value={index}
-                            id={`sortOption-${index}`}
+                <ButtonGroup className={styles.sortGroup}>
+                    {Object.keys(sortBy).map((sortByOption) => (
+                        <Button
+                            key={sortBy[sortByOption]} // The key is the value corresponding to the specific sortOption key (using bracket notation) - Ex. "sort_by=best_match&limit=20"
                             variant="outline-light"
-                            className={styles.sortButton}
+                            className={sortOptionStyle(sortBy[sortByOption])} // Passing the value corresponding to the specific key, to the sortOptionStyle function which changes the class of the button
+                            onClick={() => {
+                                handleSortingOption(sortBy[sortByOption])
+                            }}
                         >
-                            {Object.keys(sortOption)}
-                        </ToggleButton>
+                            {sortByOption}
+                        </Button>
                     ))}
-                </ToggleButtonGroup>
+                </ButtonGroup>
             </div>
-            <div>
+            <form onSubmit={handleSubmit}>
                 <div className={styles.searchSection}>
-                    <input placeholder="Search Businesses" className={styles.searchField}/>
-                    <input placeholder="Where?" className={styles.searchField}/>
+                    <input
+                        type="text"
+                        placeholder="Search Businesses"
+                        className={styles.searchField}
+                        value={searchTerm}
+                        onChange={handleSearchTerm}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Where?"
+                        className={styles.searchField}
+                        value={searchLocation}
+                        onChange={handleSearchLocation}
+                    />
                 </div>
-                <Button className={styles.searchButton}>Let's Go</Button>
-            </div>
-        </div>
+                <Button type="submit" className={styles.searchButton}>Let's Go</Button>
+            </form>
+        </div> // The search terms + button need to be wrapped in a <form>. Then the "onSubmit" will be in the opening tag of the <form>
     )
 };
 
